049eff87ae0322cc01c69b0df1272cc1b0ec26e1
JaeYoung <tekken5953@naver.com>
2022-08-03 08:58:08 +0900

    dd

---
+++ app/src/main/java/app/snfood/DetailAcitivity.java
@@ -0,0 +1,14 @@
+package app.snfood;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailAcitivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detail_acitivity);
+    }
+}
